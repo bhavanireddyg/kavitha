41c20b6e86b5a01c88eec520691d9ecf0fda3368
kavitha <bhavani.reddy2021@vitstudent.ac.in>
2024-05-02 01:12:30 +0530

    added akshith program

---
+++ akshith.java
@@ -0,0 +1,5 @@
+public class akshith{
+    public static void main(String[] args) {
+        System.out.println("Hello from Bhavani.java!");
+    }
+}
---
+++ kavitha.java
@@ -0,0 +1,5 @@
+public class Kavitha{
+    public static void main(String[] args) {
+        System.out.println("Hello from Bhavani.java!");
+    }
+}
